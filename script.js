d5d4ecaf82cce84dc0536dad3d64a527fc937fb4
gviva <viviane@guenth.art>
2022-10-18 00:13:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,115 @@
+//smooth scroll to anchor points
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        //console.log(this.getAttribute('href'));
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior : 'smooth'
+        });
+    });
+});
+ 
+//turn overlay on
+document.querySelectorAll(".work").forEach(work => {
+    work.addEventListener('click', function(e){
+        e.preventDefault();
+        document.getElementById("overlay").style.display = "block";
+    });
+});
+
+//fill individual overlay
+document.querySelectorAll('.work').forEach(work => {
+    work.addEventListener('click', function(e){
+        e.preventDefault();
+
+        let obj = works.find(o => o[this.getAttribute('id')]); //find work with ID
+
+        document.getElementById("txt-title").innerHTML = obj[this.getAttribute('id')].title; //get work's title
+        document.getElementById("txt-content").innerHTML = obj[this.getAttribute('id')].text; //get work's text
+
+        let objLength = Object.keys(obj[this.getAttribute('id')]).length;
+        document.getElementById("art-info").innerHTML = ""; //clear previous artwork info
+        document.getElementById("images").innerHTML = ""; //clear previous media
+
+        for(let i = 2; i < objLength; i++){
+            document.getElementById("images").innerHTML += obj[this.getAttribute('id')][i]; //fill in images & videos
+        }
+
+        //if inspo section
+        if(this.getAttribute('id') == "inspo"){
+            document.getElementById("images").innerHTML = "Loading..."; //clear previous media
+            document.getElementById("art-info").innerHTML = ""; //clear previous artwork info
+            startMET();
+        }
+    });
+});
+
+function startMET(){
+    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects').then(function (response) {
+        // The API call was successful!
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+        // This is the JSON from our response
+        let index = Math.round(Math.random() * (data.total-1))
+        getArtwork(data.objectIDs[index]);
+
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
+}
+
+function getArtwork(artworkID){
+    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + artworkID).then(function (response) {
+        // The API call was successful!
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+        // This is the JSON from our response
+        let artworkName = data.objectName;
+        let artist = data.artistDisplayName;
+        let artImg = data.primaryImage;
+
+        //check for completeness
+        if(artImg === ""){
+            startMET();
+            return;
+        }
+        if(artworkName === ""){
+            artworkName = "Unknown";
+        }
+        if(artist === ""){
+            artist = "unknown";
+        }
+
+        document.getElementById("images").innerHTML = "<img src=\"" + artImg + "\" class=\"image\">";
+        document.getElementById("art-info").innerHTML = "<div id=\"txt-content\">"+ artworkName + " by " + artist + "</div>"; //fill in images & videos
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+        startMET();
+    });
+}
+
+//turn overlay off
+document.querySelectorAll("#close").forEach(overlay => {
+    overlay.addEventListener('click', function(e){
+        e.preventDefault();
+        document.getElementById("overlay").style.display = "none";
+        document.getElementById("images").innerHTML = ""; //clear previous media
+    });
+});
+
+//sound effect on click
+var audio = new Audio("./assets/mixkit-click.mp3");
+
+document.onclick = function() {
+  audio.play();
+}
---
+++ works.js
@@ -0,0 +1,154 @@
+let works = [
+    //2022
+    {twistedSpines: {
+        title: "Twisted Spines",
+        text: "This project displays a museum filled with medical corsets to treat scoliosis. The main purpose was to learn the basics of Cinema 4D.<br><br><br>Pictures coming soon.",
+        2: "<video autoplay loop src=\"./vid/twistedSpines_vid_01.mp4\" class=\"image\">",
+        3: "<img src=\"./img/twistedSpines_img_01.png\" class=\"image\">",
+        4: "<img src=\"./img/twistedSpines_img_02.png\" class=\"image\">",
+        5: "<img src=\"./img/twistedSpines_img_03.png\" class=\"image\">",
+        6: "<img src=\"./img/twistedSpines_img_04.png\" class=\"image\">",
+        7: "<img src=\"./img/twistedSpines_img_05.png\" class=\"image\">",
+        8: "<img src=\"./img/twistedSpines_img_06.png\" class=\"image\">",
+        9: "<img src=\"./img/twistedSpines_img_07.png\" class=\"image\">",
+        10: "<img src=\"./img/twistedSpines_img_08.png\" class=\"image\">",
+        11: "<img src=\"./img/twistedSpines_img_09.png\" class=\"image\">",
+        12: "<img src=\"./img/twistedSpines_img_10.png\" class=\"image\">",
+        13: "<img src=\"./img/twistedSpines_img_11.png\" class=\"image\">",
+        14: "<img src=\"./img/twistedSpines_img_12.png\" class=\"image\">"
+    }}, 
+    {organicSystems: {
+        title: "Organic systems",
+        text: "L-systems created with the JavaScript Library p5.<br><br><br>Pictures coming soon.",
+        2: "<img src=\"\" class=\"image\">"
+    }},
+    {europesSeaCondition: {
+        title: "Europe's sea condition",
+        text: "This poster visualizes UNICEF's data about the sea condition of a selection of European countries. The poster is enhanced by AR with the app \"Artivive\".",
+        2: "<img src=\"./img/europesSeaCondition_img_01.png\" class=\"image\">",
+        3: "<img src=\"./img/europesSeaCondition_img_02.png\" class=\"image\">"
+    }},
+    {snailing: {
+        title: "Snailing",
+        text: "Created with the JavaScript Library p5.",
+        2: "<video autoplay loop src=\"./vid/snailing_vid_01.mp4\" class=\"image\">",
+        3: "<img src=\"./img/snailing_img_01.png\" class=\"image\">",
+        4: "<img src=\"./img/snailing_img_02.png\" class=\"image\">",
+        5: "<video autoplay loop src=\"./vid/snailing_vid_02.mp4\" class=\"image\">",
+        6: "<img src=\"./img/snailing_img_03.png\" class=\"image\">",
+        7: "<img src=\"./img/snailing_img_04.png\" class=\"image\">",
+        8: "<img src=\"./img/snailing_img_05.png\" class=\"image\">",
+        9: "<img src=\"./img/snailing_img_06.png\" class=\"image\">"
+    }},
+    {viviLogo: {
+        title: "VIVI Logo",
+        text: "Originally I needed a stamp to mark my ceramics. This was an opportunity to design my logo. I experimented with the shapes of my nickname \"Vivi\" with a pencil on paper. The design was finalized in Illustrator.",
+        2: "<img src=\"./img/viviLogo_img_01.png\" class=\"image\">",
+        3: "<img src=\"./img/viviLogo_img_02.png\" class=\"image\">",
+        4: "<img src=\"./img/viviLogo_img_03.JPG\" class=\"image\">",
+        5: "<img src=\"./img/viviLogo_img_04.JPG\" class=\"image\">"
+    }},
+    {wilmaBee: {
+        title: "Wilma Bee",
+        text: "Within two weeks this animation about main character Wilma Bee's life and dreams was created. Procreate and After Effects were used.",
+        2: "<video autoplay loop controls src=\"./vid/wilmaBee_vid_01.mp4\" class=\"image\">",
+        3: "<img src=\"./img/wilmaBee_img_01.png\" class=\"image\">",
+        4: "<img src=\"./img/wilmaBee_img_02.png\" class=\"image\">",
+        5: "<img src=\"./img/wilmaBee_img_03.png\" class=\"image\">",
+        6: "<img src=\"./img/wilmaBee_img_04.png\" class=\"image\">",
+        7: "<img src=\"./img/wilmaBee_img_05.png\" class=\"image\">"
+    }},
+    {dissolvingMind: {
+        title: "dissolving Mind",
+        text: "This is the first silk print I did from beginning to end.",
+        2: "<img src=\"./img/dissolvingMind_img_01.JPG\" class=\"image\">",
+        3: "<img src=\"./img/dissolvingMind_img_02.JPG\" class=\"image\">",
+        4: "<img src=\"./img/dissolvingMind_img_03.JPG\" class=\"image\">",
+        5: "<img src=\"./img/dissolvingMind_img_04.JPG\" class=\"image\">"
+    }},
+
+    //2021
+    {mariana: {
+        title: "Mariana",
+        text: "Mariana is a 2D underwater game. The goal is to guide the yellow submarine to the surface without running out of oxygen or getting too much damage by enemies. You can move left and right and activate a shield. <br>This is a game I worked on with Lina Haag and Silvan Philipp. It's the result of a seven-day class at university. I was responsible for programming. It was created using Unity.<br>Use this <a href=\"https://phase02.itch.io/mariana\" class=\"link-obj link-txt\" target=\"_blank\" rel=\"noopener noreferrer\">LINK</a> to play online.<br><br><br> Pictures coming soon.",
+        2: "<img src=\"\" class=\"image\">"
+    }},
+    {processingAPoster: {
+        title: "Processing a poster",
+        text: "This is a poster about designer Casey Reas. The first video is the final version. A portrait of the designer, one of his works and a quote were used. It was made using Processing and Premiere Pro for the quote.",
+        2: "<video autoplay loop src=\"./vid/processingAPoster_vid_01.mp4\" class=\"image\">",
+        3: "<img src=\"./img/reas_img_01.png\" class=\"image\">",
+        4: "<img src=\"./img/reas_img_02.png\" class=\"image\">",
+        5: "<video autoplay loop src=\"./vid/processingAPoster_vid_02.mp4\" class=\"image\">",
+        6: "<img src=\"./img/reas_img_03.png\" class=\"image\">"
+    }},
+    {nameComposition: {
+        title: "Name composition",
+        text: "These posters are the result of practicing composition with my first name \"Viviane\".",
+        2: "<img src=\"./img/nameComposition_img_01.png\" class=\"image\">",
+        3: "<img src=\"./img/nameComposition_img_02.png\" class=\"image\">"
+    }},
+    {portraitStudy: {
+        title: "Portrait study",
+        text: "The goal of this drawing is to practice working with colored pencils.",
+        2: "<img src=\"./img/portraitStudy_img_01.JPG\" class=\"image\">",
+        3: "<img src=\"./img/portraitStudy_img_02.JPG\" class=\"image\">",
+        4: "<img src=\"./img/portraitStudy_img_03.JPG\" class=\"image\">",
+        5: "<img src=\"./img/portraitStudy_img_04.JPG\" class=\"image\">",
+        6: "<img src=\"./img/portraitStudy_img_05.JPG\" class=\"image\">",
+        7: "<img src=\"./img/portraitStudy_img_06.JPG\" class=\"image\">"
+    }},
+    {fierceness: {
+        title: "Fierceness",
+        text: "A quick illustrating exercise made with Procreate.",
+        2: "<img src=\"./img/fierceness_img_01.png\" class=\"image\">"
+    }},
+
+    //2020
+    {aktzeichnen: {
+        title: "Aktzeichnen",
+        text: "Nude drawing practice. The goal was to capture movements and facets of the body within various timeframes.",
+        2: "<img src=\"./img/aktzeichnen_img_01.JPG\" class=\"image\">",
+        3: "<img src=\"./img/aktzeichnen_img_02.JPG\" class=\"image\">",
+        4: "<img src=\"./img/aktzeichnen_img_03.JPG\" class=\"image\">"
+    }},
+
+    //2019
+    {figuerlichesZeichnen: {
+        title: "Figürliches Zeichnen",
+        text: "Figure drawing practice. The goal was to capture different amounts of details in various timeframes.",
+        2: "<img src=\"./img/figuerlichesZeichnen_img_01.JPG\" class=\"image\">",
+        3: "<img src=\"./img/figuerlichesZeichnen_img_02.JPG\" class=\"image\">",
+        4: "<img src=\"./img/figuerlichesZeichnen_img_03.JPG\" class=\"image\">",
+        5: "<img src=\"./img/figuerlichesZeichnen_img_04.JPG\" class=\"image\">",
+        6: "<img src=\"./img/figuerlichesZeichnen_img_05.JPG\" class=\"image\">"
+    }},
+
+    //2018
+    {bamboo: {
+        title: "Bamboo",
+        text: "The \"BAMBOO\" series consists of three chair miniatures made from plastic bottles, bamboo skewers and nylon thread. A foldable flyer was created for the main model \"BAMBOO 01\".",
+        2: "<img src=\"./img/bamboo_img_01.jpg\" class=\"image\">",
+        3: "<img src=\"./img/bamboo_img_02.jpg\" class=\"image\">"
+    }},
+
+    //2017
+    {zwischenBitterUndSuess: {
+        title: "Zwischen bitter und suess",
+        text: "This is my final project for high school. I made a dress, headdress, petticoat and bloomers. The \"Lolita\" and \"Gothic\" subcultures were analyzed and the knowledge used to create these pieces.",
+        2: "<img src=\"./img/zwischenBitterUndSuess_img_01.jpg\" class=\"image\">",
+        3: "<img src=\"./img/zwischenBitterUndSuess_img_02.jpg\" class=\"image\">",
+        4: "<img src=\"./img/zwischenBitterUndSuess_img_03.jpg\" class=\"image\">",
+        5: "<img src=\"./img/zwischenBitterUndSuess_img_04.jpg\" class=\"image\">",
+        6: "<img src=\"./img/zwischenBitterUndSuess_img_05.jpg\" class=\"image\">",
+        7: "<img src=\"./img/zwischenBitterUndSuess_img_06.jpg\" class=\"image\">",
+        8: "<img src=\"./img/zwischenBitterUndSuess_img_07.jpg\" class=\"image\">"
+    }},
+
+    //inspo
+    {inspo: {
+        title: "Inspo",
+        text: "This section uses <a href=\"https://www.metmuseum.org/\" class=\"link-obj link-txt\" target=\"_blank\" rel=\"noopener noreferrer\">The Met</a> Collection API to randomly display one of their artworks. You can use this as an inspiration for your current project, to practice sketching or for whatever you feel like.",
+        2: "<img src=\"\" class=\"image\">"
+    }},
+];
